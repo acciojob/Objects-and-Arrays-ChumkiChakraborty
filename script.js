34c237878d6f13d7b96ad1800c561548981cfaab
ChumkiChakraborty <137317244+ChumkiChakraborty@users.noreply.github.com>
2023-08-18 21:12:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,11 +4,10 @@ const person = {
   name: "John Doe",
   age: 80,
 };
-
+const team = players; // Create a reference to the 'players' array
+const team1 = players.slice(); //Create a copy of the 'players' array
+const cap1 = { ...person }; // Create a copy of the 'person' object
+console.log("team:", team);
+    console.log("team1:", team1);
+    console.log("cap1:", cap1);
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
