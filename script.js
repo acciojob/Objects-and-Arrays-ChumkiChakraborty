45c13baa529a54432544eee9d09bdd1f4ad55ba6
ChumkiChakraborty <137317244+ChumkiChakraborty@users.noreply.github.com>
2023-08-18 21:14:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,3 +11,9 @@ console.log("team:", team);
     console.log("team1:", team1);
     console.log("cap1:", cap1);
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+
+window.players = players;
+window.person = person;
+window.team = team;
+window.team1 = team1;
+window.cap1 = cap1;
